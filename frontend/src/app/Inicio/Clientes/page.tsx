e969623e98266954e231108fb48e4683fb8f9c35
Karen Moreno <utm22030783@utma.edu.mx>
2025-01-29 23:50:21 -0600

    Editar Iniciar Sesion

--- frontend/src/app/Inicio/Clientes/page.tsx
+++ frontend/src/app/Inicio/Clientes/page.tsx
@@ -2,7 +2,6 @@ export default function Clientes() {
     return (
           <main className="flex-grow p-6 bg-white">
             <p className="text-center text-xl font-semibold text-black ">Buscar cliente</p>
-
            {/* Buttons */}
            <div className="flex flex-col items-center space-y-4">
            <button className="bg-[#6aa3af] text-white py-2 px-4 rounded hover:bg-[#558c98]">INGRESA EL NOMBRE DEL CLIENTE</button>
--- frontend/src/app/Inicio/layout.tsx
+++ frontend/src/app/Inicio/layout.tsx
@@ -1,21 +1,30 @@
 import { ReactNode } from "react";
+import Image from 'next/image';
 
 export default function Layout({ children }: { children: ReactNode }) {
-    return(
+    return (
         <div className="flex h-screen">
-            {/* Sidebar */}
-            <aside className="w-1/5 bg-[#4ab9d2] text-white flex flex-col">
+              {/* Sidebar */}
+              <aside className="w-1/5 bg-blue-800 text-white flex flex-col" style={{ backgroundImage: 'url("/Imagen1.svg")', backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'left' }}>
+
                 <div className="text-center text-lg font-bold p-4 border-b border-gray-300">
-                    PANEL DE OPCIONES
+                    INICIO
                 </div>
+
                 <nav className="flex-grow">
                     <ul className="space-y-4 p-4">
-                        <li className="hover:bg-[#6a87af] p-2 rounded cursor-pointer">CLIENTES</li>
-                        <li className="hover:bg-[#6aa3af] p-2 rounded cursor-pointer">SERVICIOS</li>
-                        <li className="hover:bg-[#6aa3af] p-2 rounded cursor-pointer">CUENTAS</li>
-                        <li className="hover:bg-[#6aa3af] p-2 rounded cursor-pointer">DEUDAS</li>
-                        <li className="hover:bg-[#6aa3af] p-2 rounded cursor-pointer">CARIÑAN</li>
-
+                        <li className="hover:bg-[#6a87af] p-2 rounded cursor-pointer flex items-center gap-2 text-center">
+                            <Image src="/user.svg" alt="Clientes Icon" width={20} height={20} />CLIENTES
+                        </li>
+                        <li className="hover:bg-[#6aa3af] p-2 rounded cursor-pointer flex items-center gap-2 text-center">
+                            <Image src="/services.svg" alt="Servicios Icon" width={20} height={20} />SERVICIOS
+                        </li>
+                        <li className="hover:bg-[#6aa3af] p-2 rounded cursor-pointer flex items-center gap-2 text-center">
+                            <Image src="/dollar.svg" alt="Cuentas Icon" width={20} height={20}/>CUENTAS
+                        </li>
+                        <li className="hover:bg-[#6aa3af] p-2 rounded cursor-pointer flex items-center gap-2 text-center">
+                            <Image src="/rural.svg" alt="Cariñan Icon" width={20} height={20} />CARIÑAN
+                        </li>
                     </ul>
                 </nav>
             </aside>
@@ -23,7 +32,8 @@ export default function Layout({ children }: { children: ReactNode }) {
             {/* Main Content Area */}
             <div className="flex-grow flex flex-col">
                 {/* Top Bar */}
-                <header className="w-full bg-[#195c97] text-white 700 p-4 text-lg font-bold shadow-md">
+                <header className="w-full bg-[#529fe4] text-white p-4 text-lg font-bold shadow-md flex items-center gap-4">
+                    <Image src="/inagua.svg" alt="inagua Icon" width={20} height={20} className="w-auto h-auto"/>
                     EFICOBRO
                 </header>
 
@@ -32,3 +42,5 @@ export default function Layout({ children }: { children: ReactNode }) {
         </div>
     )
 }
+
+export function Page() {}
