de2ca4752d0c82e8ec57d8299ae9fa3500aca198
MontsVargas <utm22030776@utma.edu.mx>
2025-01-31 22:24:42 -0600

    rutas

---
+++ Backend/src/routes/rutasdeUsuario.ts
@@ -0,0 +1,19 @@
+import { Request, Response, Router } from "express";
+import { registro, iniciarSesion, cerrarSesion } from "../controllers/usuarios controller";
+import authMiddleware from "../helpers/authMiddleware";
+
+const rutas= Router();
+
+rutas.post('/registro', (req: Request, res: Response) => {
+    registro(req, res);
+});
+
+rutas.post('/iniciarSesion', (req: Request, res: Response) => {
+    iniciarSesion(req, res);
+});
+
+rutas.post('/cerrarSesion', authMiddleware, (req: Request, res: Response) => {
+    cerrarSesion(req, res);
+});
+
+export default rutas;
