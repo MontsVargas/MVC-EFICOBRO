65689c38ac2dd210ac63f9047b22dc846c2aa79d
MontsVargas <utm22030776@utma.edu.mx>
2025-02-15 22:22:35 -0600

    posibles schemas

---
+++ frontend/src/schemas/info-cliente-nuevo-schema.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+const clienteNuevoSchema = z.object ({
+    nombre: z.string().min(64, { message: "El nombre es obligatorio" }),
+    direccion: z.string().min(64, { message: "La dirección es obligatoria" }),
+    telefono: z.string().min(12, { message: "El numero de teléfono es obligatorio" }),
+    nombreDependencia: z.string().min(64, { message: "El nombre de la dependencia es obligatorio" }),
+    contrato: z.string().min(20, { message: "El contrato es obligatorio" }),
+    numeroMedidor: z.string().min(20,{ message: "El número del medidor es obligatorio" }),
+  });
+// aqui no puedo poner el export defaut creo porque debo llamarla desde la carpeta que ponga kath 
+export default clienteNuevoSchema;
