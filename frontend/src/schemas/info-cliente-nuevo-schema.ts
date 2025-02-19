7f7b0529b0c82766076facc515b87e717e622c9a
Karen Moreno <utm22030783@utma.edu.mx>
2025-02-18 22:37:07 -0600

    page.tsx de clientes nuevos

--- frontend/src/schemas/info-cliente-nuevo-schema.ts
+++ frontend/src/schemas/info-cliente-nuevo-schema.ts
@@ -5,8 +5,7 @@ const clienteNuevoSchema = z.object ({
     direccion: z.string().min(64, { message: "La dirección es obligatoria" }),
     telefono: z.string().min(12, { message: "El numero de teléfono es obligatorio" }),
     nombreDependencia: z.string().min(64, { message: "El nombre de la dependencia es obligatorio" }),
-    contrato: z.string().min(20, { message: "El contrato es obligatorio" }),
-    numeroMedidor: z.string().min(20,{ message: "El número del medidor es obligatorio" }),
+    id_medidor: z.string().min(20,{ message: "El número del medidor es obligatorio" }),
   });
 // aqui no puedo poner el export defaut creo porque debo llamarla desde la carpeta que ponga kath 
 export default clienteNuevoSchema;
