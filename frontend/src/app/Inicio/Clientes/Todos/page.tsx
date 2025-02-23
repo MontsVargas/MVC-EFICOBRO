2f39932e896b0d64fce9d81a8c76af7ffcd2c31f
Karen Moreno <utm22030783@utma.edu.mx>
2025-02-22 21:25:07 -0600

    Se agrega ruta para mostrar todos los clientes

---
+++ frontend/src/app/Inicio/Clientes/Todos/page.tsx
@@ -0,0 +1,5 @@
+export default function Todos() {
+    return (
+        <p className="text-black 600 mt-2">Holic:</p>
+    );
+  }
--- frontend/src/app/Inicio/Clientes/page.tsx
+++ frontend/src/app/Inicio/Clientes/page.tsx
@@ -6,7 +6,9 @@ export default function Clientes() {
            {/* Buttons */}
            <div className="flex flex-col items-center space-y-4">
            <button className="bg-[#6aa3af] text-white py-2 px-4 rounded hover:bg-[#558c98]">INGRESA EL NOMBRE DEL CLIENTE</button>
-            <button className="bg-[#6aa3af] text-white py-2 px-4 rounded hover:bg-[#558c98]">MOSTRAR RESULTADOS</button>
+           <Link href="/Inicio/Clientes/Todos">
+            <button className="bg-[#6aa3af] text-white py-2 px-4 rounded hover:bg-[#558c98]">MOSTRAR TODOS LOS CLIENTES</button>
+            </Link>
             <Link href="/Inicio/Clientes-Nuevos">
             <button className="bg-[#6aa3af] text-white py-2 px-4 rounded hover:bg-[#558c98]">AGREGAR CLIENTE</button>
             </Link>
