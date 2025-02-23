7c88b898f677d4f527ed7a4a5ff4f51654a7428f
Karen Moreno <utm22030783@utma.edu.mx>
2025-02-22 21:54:39 -0600

    Tabla de clientes

--- frontend/src/app/Inicio/Clientes/Todos/page.tsx
+++ frontend/src/app/Inicio/Clientes/Todos/page.tsx
@@ -1,23 +1,30 @@
 import clientes from "@/mocks/clientes";
-type Cliente = {
-    nombre: string;
-    direccion: string;
-    contrato_id?: number;
-  };
-  export default function Todos (){
-    return (
-      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
-        <h1 className="text-3xl font-bold text-gray-800">Lista de Clientes</h1>
-        <ul className="mt-4 w-full max-w-md">
-          {clientes.map((cliente, index) => (
-            <li key={index} className="bg-white p-4 rounded-lg shadow-md mb-2">
-              <p className="text-lg font-semibold">{cliente.nombre}</p>
-              <p className="text-gray-600">Dirección: {cliente.direccion}</p>
-              {cliente.contrato_id && <p className="text-gray-500">Contrato ID: {cliente.contrato_id}</p>}
-            </li>
-          ))}
-        </ul>
+
+export default function Todos() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+      <h1 className="text-3xl font-bold text-gray-800 mb-4">Todos los Clientes</h1>
+      
+      <div className="w-full max-w-4xl">
+        <table className="w-full bg-blue-400 shadow-md rounded-lg overflow-hidden">
+          <thead className="bg-blue-200-200">
+            <tr>
+              <th className="p-3 text-left">Nombre</th>
+              <th className="p-3 text-left">Dirección</th>
+              <th className="p-3 text-left">Contrato ID</th>
+            </tr>
+          </thead>
+          <tbody>
+            {clientes.map((cliente, index) => (
+              <tr key={index} className="border-b">
+                <td className="p-3">{cliente.nombre}</td>
+                <td className="p-3">{cliente.direccion}</td>
+                <td className="p-3">{cliente.contrato_id ? cliente.contrato_id : "Sin Contrato"}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
