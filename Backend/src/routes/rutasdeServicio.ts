77ef22b3d65e0a6bae063293c94344440d9686b8
MontsVargas <utm22030776@utma.edu.mx>
2025-03-10 19:41:11 -0600

    cambios controller servidor

--- Backend/src/routes/rutasdeServicio.ts
+++ Backend/src/routes/rutasdeServicio.ts
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import { obtenerServicios, buscarServiciosPorTipo, obtenerPlantas } from "../controllers/serviciosController"; 
 
-const router = Router();
+const rutas = Router();
 
 // Rutas para servicios
-router.get("/servicios", obtenerServicios);
-router.get("/servicios/tipo", buscarServiciosPorTipo);
+rutas.get("/servicios", obtenerServicios);
+rutas.get("/servicios/tipo", buscarServiciosPorTipo);
 
 // Ruta para obtener plantas de la empresa
-router.get("/plantas", obtenerPlantas);
+rutas.get("/plantas", obtenerPlantas);
 
-export default router;
+export default rutas;
