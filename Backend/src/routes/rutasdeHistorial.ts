e0e68c3874749ccc57ad1ddd99c77b00dd380bce
MontsVargas <utm22030776@utma.edu.mx>
2025-03-27 20:12:06 -0600

    ruta y endpoint de historial falto el json

--- Backend/src/routes/rutasdeHistorial.ts
+++ Backend/src/routes/rutasdeHistorial.ts
@@ -4,8 +4,10 @@ import { Historial } from "../controllers/historialcontroller";
 const rutas = Router();
 
 //RUTA DE GET PARA OBTENER EL HISTORIAL DE COMPRAS DE CADA CLIENTE
-rutas.get("/historial/:id", (req: Request, res: Response) => {
-    Historial(req, res);
+rutas.get("/historial/:id", async (req: Request, res: Response) => {
+    const id = decodeURIComponent(req.params.id);
+    console.log("ID recibido:", id);
+    await Historial(req, res);
 });
 
 export default rutas; 
