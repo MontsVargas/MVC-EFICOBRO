8a958a443da27b7cba2ce7181f310b8817f8808c
Karen Moreno <utm22030783@utma.edu.mx>
2025-04-02 13:30:35 -0600

    schema servicio zod

--- frontend/src/schemas/contrato-de-servicio-schema.ts
+++ frontend/src/schemas/contrato-de-servicio-schema.ts
@@ -7,12 +7,7 @@ const contratoServicioSchema = z.object ({
       .string()
       .regex(/^\d{2}\/\d{2}\/\d{4}$/, { message: "La fecha debe tener el formato dd/mm/aaaa." }),
     cifraServicio: z.string().min(1, { message: "La cifra del servicio es obligatoria." }),
-    costoServicio: z
-      .string()
-      .regex(/^\d+(\.\d{1,2})?$/, { message: "El costo del servicio debe ser un número válido." }),
-    direccion: z.string().min(1, { message: "La dirección es obligatoria." }),
     planta: z.string().min(1, { message: "La planta es obligatoria." }),
-
 
 })
 export default contratoServicioSchema;
